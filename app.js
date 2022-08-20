5b43298de22ffb19fb7bf07f79200e57df3d8ebb
emilianoolbe <emilianoolbe@gmail.com>
2022-08-20 19:23:55 -0300

    app actualizado

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname,"./views/login.html"))
 });
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/carrito.html"))
+});
+
+app.get("/productos", (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/productos.html"))
+});
