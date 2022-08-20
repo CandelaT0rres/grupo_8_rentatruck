4eda26884e8bcb8688dd00d173c2ad39f3f3a981
emilianoolbe <emilianoolbe@gmail.com>
2022-08-20 18:10:22 -0300

    primera version

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname,"./public")));
+
+app.listen(3005, ()=>{
+    console.log("servidor corriendo puerto 3005")});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
