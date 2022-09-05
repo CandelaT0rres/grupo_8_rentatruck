99630d82555cc6b54ac4591121ba2327f7416a90
CandelaT0rres <candetorretas@gmail.com>
2022-09-05 20:38:27 -0300

    FIN?

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,"./public")));
 
-app.listen(3005, ()=>{
-    console.log("servidor corriendo puerto 3005")});
+/*app.listen(3005, ()=>{
+   console.log("servidor corriendo puerto 3005")});
+*/
+
+app.listen(process.env.PORT || 3005, function() {
+    console.log("Servidor corriendo en el puerto 3005");
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"./views/home.html"))
