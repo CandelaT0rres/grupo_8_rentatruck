9696883da75cf3b47da07d477d232ec07b22e638
emilianoolbe <emilianoolbe@gmail.com>
2022-09-25 23:09:48 -0300

    vistas cargar editar producto - arreglo enrutadores - controladores - multer

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,7 +4,7 @@ const productsController = {
     productos: (req , res) => {
        res.render ('./products/productos', {productos: productosData});
     },
-
+    
     carrito: (req , res) => {
       var totalProductos = 0;
       for (d of productosData) {
@@ -12,11 +12,46 @@ const productsController = {
       };
         res.render ('./products/carrito', {productos: productosData, total: totalProductos})
      },
+     
      cargar: (req, res) => {
       res.render('./products/cargar')
      },
+
+     guardar: (req, res) =>{
+
+      let idNuevo = (productosData[productosData.length - 1].id) + 1;
+
+      let camionNuevo = {
+         id: idNuevo,
+         nombre: req.body.nombre,
+         marca: req.body.marca,
+         modelo: req.body.modelo,
+         tipoC: req.body.tipoC,
+         precioKm: parseInt(req.body.precioKm),
+         rutaImg: 'camion1.jpg',
+         origen: req.body.origen,
+         recorrido: req.body.recorrido
+         }
+      productosData.push(camionNuevo);
+      res.redirect('/');
+     },
+
      editar: (req, res) => {
-      res.render('./products/editar')
+      let camionBuscado = null;
+      for(cadaElemento of productosData){
+         if (cadaElemento.id == req.params.id)
+         camionBuscado = cadaElemento;
+         
+      }
+      if (camionBuscado != null) {
+         res.render('./products/editar', {producto: camionBuscado});
+      }else{
+         res.send('Error camión no encontrado');
+      };
+     },
+
+     actualizar: (req, res) => {
+
      }
 };
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,14 +1,38 @@
+//Importo Express + Ejecuto método Router
 const express = require('express');
 const router = express.Router();
+
+//Importo Path
+const path = require('path');
+
+//Importo multer + seteo multer
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/img'))
+    },
+    filename: (req, file, cb) =>{
+        let nombreImg = 'camiones-' + Date.now() + path.extname(file.originalname);
+        cb(null, nombreImg);
+    }
+});
+const upload = multer({storage});
+
+//Importo Controlador
 const controller = require('../controllers/productsController');
 
-
+//Vista Productos
 router.get('/productos', controller.productos);
-router.get('/carrito',  controller.carrito);
-router.get('/cargar',  controller.cargar);
-router.get('/editar',  controller.editar);
+
+//Vista Carrito
+router.get('/carrito', controller.carrito);
+
+//Cargar producto
+router.get('/cargar', controller.cargar);
+router.post('/cargar', controller.guardar);
+
+//Editar producto
+router.get('/editar/:id', controller.editar);
+router.put('/editar/:id', controller.actualizar);
 
 module.exports = router;
- 
-
-
