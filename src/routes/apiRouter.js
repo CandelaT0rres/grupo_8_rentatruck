fe635182e1a51ec01c17ec0592d473922c36eb48
Bernardo Franco <berniifranco@gmail.com>
2022-12-15 23:32:34 -0300

    api productos

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/ApiController');
-const router = require('./homeRouter');
 
 router.get('/list', controller.productsList);
 
