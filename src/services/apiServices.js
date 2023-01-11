b992ef38357279fd9923ff314a395050140d4697
Bernardo Franco <berniifranco@gmail.com>
2023-01-11 20:49:34 -0300

    endpoin inutilizado eliminado

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -19,27 +19,6 @@ let vehiculos = {
         let cantidad = await db.Vehiculo.findAll();
         return cantidad.length;
     },
-    countByCategory: async() => {
-        let VehiculoCategoria = await db.Vehiculo.findAll({include: [{association: 'tipo_mercaderia'}]});
-        let categorias = [[], [], [], [], [], []];
-       
-        for(x of VehiculoCategoria){
-            if (x.tipo_mercaderia.id == 1) {
-                categorias[0].push(x)
-            }else if (x.tipo_mercaderia.id == 2){
-                categorias[1].push(x)
-            }else if (x.tipo_mercaderia.id == 3){
-                categorias[2].push(x)
-            }else if (x.tipo_mercaderia.id == 4){
-                categorias[3].push(x)
-            }else if (x.tipo_mercaderia.id == 5){
-                categorias[4].push(x)
-            }else{
-                categorias[5].push(x)
-            };
-        };
-        return categorias;
-    },
     allVehiculos: async () => {
         return await db.Vehiculo.findAll({include: [{association: 'marcas'}, {association: 'tipo_mercaderia'}]});
     }
